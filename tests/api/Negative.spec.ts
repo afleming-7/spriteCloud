de232d80ec90cf8e40592b408d972ec6a3b053aa
afleming-7 <alix75@gmail.com>
2025-09-04 16:07:51 +0200

    Updated Readme and added reformatted test case for Negative tests to show parallel test runs in single file

--- tests/api/Negative.spec.ts
+++ tests/api/Negative.spec.ts
@@ -1,6 +1,17 @@
 import { test, expect, APIRequestContext, APIResponse } from "@playwright/test";
 import { createApiContext, API_BASE } from "../../utils/APIClient.js";
 
+test.describe.configure({ mode: "parallel" });
+
+test("@API Negative: get non-existing user", async () => {
+  const context: APIRequestContext = await createApiContext();
+  const response: APIResponse = await context.get(`${API_BASE}/users/23`);
+
+  expect([404, 401]).toContain(response.status());
+
+  await context.dispose();
+});
+
 test("@API Negative: login fails without password", async () => {
   const context: APIRequestContext = await createApiContext();
   const response: APIResponse = await context.post(`${API_BASE}/login`, {
